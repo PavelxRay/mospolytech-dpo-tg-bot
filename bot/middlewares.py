57b04685ab538b513d3f6949be773ae3a6f666c9
Chernenkiy <nikmbogdanov@gmail.com>
2024-11-08 00:41:43 +0300

    Adding database session middleware

--- bot/middlewares.py
+++ bot/middlewares.py
@@ -3,14 +3,18 @@ from typing import Any, Callable, Awaitable
 from aiogram import Dispatcher
 from aiogram.types import Update
 
+from database.database import database
+
 
 async def database_session_middleware(
         handler: Callable[[Update, dict[str, Any]], Awaitable[Any]],
         event: Update,
         data: dict[str, Any]
 ) -> Any:
-    data["session"] = 1
-    return await handler(event, data)
+    async with database.session() as session:
+        data["session"] = session
+        result = await handler(event, data)
+        await session.commit()
 
 
 def setup_middlewares(dp: Dispatcher):
