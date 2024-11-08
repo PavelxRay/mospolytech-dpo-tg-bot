94522c8e4963bcceea7dc89a19bc37c3458bb0c4
Ni1cky <nikmbogdanov@gmail.com>
2024-11-08 17:04:37 +0300

    Initializing person model

---
+++ database/models.py
@@ -0,0 +1,32 @@
+import datetime
+from uuid import UUID
+
+from sqlalchemy import String
+from sqlalchemy.orm import Mapped, mapped_column
+
+from database.sqlalchemy_base import Base
+
+
+class Person(Base):
+    __tablename__ = "persons"
+
+    id: Mapped[UUID] = mapped_column(primary_key=True, index=True)
+    telegram_user_id: Mapped[str] = mapped_column(unique=True, index=True)
+    dttm_created: Mapped[datetime.datetime]
+    full_name: Mapped[str]
+    birthdate: Mapped[datetime.date]
+    registration_address: Mapped[str]
+    residential_address: Mapped[str]
+    passport_number: Mapped[str] = mapped_column(String(10))
+    passport_given_by: Mapped[str]
+    passport_given_date: Mapped[datetime.date]
+    snils: Mapped[str] = mapped_column(String(11))
+    phone: Mapped[str]
+    email: Mapped[str]
+
+
+class Program(Base):
+    __tablename__ = "programs"
+
+    id: Mapped[UUID] = mapped_column(primary_key=True, index=True)
+    # all other fields
