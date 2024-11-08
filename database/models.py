f31b7f7a94c0ed050e91422343b525841aaddfa2
Chernenkiy <nikmbogdanov@gmail.com>
2024-11-08 23:15:16 +0300

    Change "persons" table schema with regard to creating "personal_info" table

--- database/models.py
+++ database/models.py
@@ -1,6 +1,5 @@
 import datetime
 
-from sqlalchemy import String
 from sqlalchemy.orm import Mapped, mapped_column
 
 from database.sqlalchemy_base import Base
@@ -12,13 +11,3 @@ class Person(Base):
     id: Mapped[int] = mapped_column(primary_key=True, index=True)
     telegram_user_id: Mapped[str] = mapped_column(unique=True, index=True)
     dttm_created: Mapped[datetime.datetime]
-    full_name: Mapped[str]
-    birthdate: Mapped[datetime.date]
-    registration_address: Mapped[str]
-    residential_address: Mapped[str]
-    passport_number: Mapped[str] = mapped_column(String(10))
-    passport_given_by: Mapped[str]
-    passport_given_date: Mapped[datetime.date]
-    snils: Mapped[str] = mapped_column(String(11))
-    phone: Mapped[str]
-    email: Mapped[str]
