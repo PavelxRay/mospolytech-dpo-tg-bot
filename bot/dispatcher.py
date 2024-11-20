a3d9efb42817fcf8d33ea9b987736f3c7ca8a708
Abdulboriy Isamidinov <iasm.78.a@gmail.com>
2024-11-20 11:54:08 +0300

    Adding a new router faq_router to the dispatcher configuration

--- bot/dispatcher.py
+++ bot/dispatcher.py
@@ -3,6 +3,7 @@ from aiogram.fsm.storage.memory import MemoryStorage
 
 from bot.handlers.commands import commands_router
 from bot.handlers.fsm.documents_fsm import documents_fsm_router
+from bot.handlers.faq import faq_router
 
 
 def setup_dispatcher() -> Dispatcher:
@@ -10,4 +11,5 @@ def setup_dispatcher() -> Dispatcher:
 
     dp.include_router(commands_router)
     dp.include_router(documents_fsm_router)
+    dp.include_router(faq_router)
     return dp
